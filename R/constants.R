db1325eeaa0d0a7e031fa8da8ab6341044c5babc
keithgw <kwilliams@redventures.com>
2020-05-27 14:37:46 -0400

    add googlesheets interaciton

--- R/constants.R
+++ R/constants.R
@@ -1,1 +1,2 @@
 EMPTY_LEDGER <- data.frame(name = c(), date = c(), transaction = c(), amount = c())
+sheet_path <- "https://docs.google.com/spreadsheets/d/1pr8_TpZQu4UGaCnPl05hqrDiRoGgPAghIFbRnivh3Wg"
